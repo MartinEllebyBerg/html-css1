ab50cc375504792cc5dbe7676a58ce7bb2d65362
MartinEllebyBerg <160470389+MartinEllebyBerg@users.noreply.github.com>
2024-09-04 09:33:11 +0200

    First

---
+++ src/main/java/com/example/htmlcss1/HtmlCss1Application.java
@@ -0,0 +1,13 @@
+package com.example.htmlcss1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlCss1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtmlCss1Application.class, args);
+    }
+
+}
